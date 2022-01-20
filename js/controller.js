3a12be9e10045a80042b81c51a891a731cbc9a59
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2022-01-20 19:43:33 -0300

    update

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,17 @@ var nome = "Renato Felix";
 var peso = 76.00;
 var altura = 1.78;
 var imc = peso / (altura**2);
+var Situacao = null;
+
+if (imc < 18.5) {
+    situacao = 'Magreza';
+} else if (imc >= 18.5 && imc <= 24.9) {
+    situacao = 'Normal';
+} else if (imc > 24.9 && imc <= 30) {
+    situacao = 'Sobrepeso';
+} else {
+    situacao = 'Obesidade';
+}
 
 var spanNome = document.getElementById("nome");
 spanNome.innerHTML = nome;
@@ -14,3 +25,6 @@ spanAltura.innerHTML = altura;
 
 var spanImc = document.getElementById("imc");
 spanImc.innerHTML = imc.toFixed(2);
+
+var spanSituacao = document.getElementById("situacao");
+spanSituacao.innerHTML = situacao;
